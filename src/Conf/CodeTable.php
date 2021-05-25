c5d47181ef04771524c816326d21d4746fb45ebe
Brian Gregg <bdgregg@pitt.edu>
2021-05-25 15:13:05 -0400

    Function description cleanup.

--- src/Conf/CodeTable.php
+++ src/Conf/CodeTable.php
@@ -26,23 +26,21 @@ class CodeTable extends LazyResource
     }
 
     /**
-    * Return a single row referring to the code table.
+    * Return a list of rows referring to the code of the rows in the table.
     *
-    * @param $code - The code in the Table we want to pull.
+    * @param string $code - The code of the row in the Table we want to pull.
     *
-    * @return object - the row in the code table.
+    * @return array $found - The rows in the code table that match the code passed in.
     */
     public function getRowByCode($code)
     {
         $found = array();
         $codeTable = json_decode($this->client->get($this->urlBase()));
-
         foreach ($codeTable->row as $row) {
             if ($row->code == $code) {
                 array_push($found,$row);
             }
         }
-
         return($found);
     }
 
